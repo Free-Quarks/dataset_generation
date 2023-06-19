59cf228c3dce62c1480dd7f2020e8b1b1faaad31
Free-Quarks <lieffers@email.arizona.edu>
2023-06-19 13:52:48 -0700

    added write support

--- dataset_generator.py
+++ dataset_generator.py
@@ -19,8 +19,8 @@ f.close()
 # This parsing struggles if trying to find line numbers, it will often return the lines in their entirety
 # however, it seems to work well for returning the name of the function that can be used to define the dynamics
 response_schemas = [
-    ResponseSchema(name="code", description="the code simulating the model"),
-    ResponseSchema(name="function_name", description="The name of the function implementating the model dynamics")
+    ResponseSchema(name="code", description="the entire code"),
+    ResponseSchema(name="function_name", description="The name of the function implementating the model")
 ]
 
 # for structured output parsing
@@ -29,7 +29,7 @@ output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
 # for structured output parsing
 format_instructions = output_parser.get_format_instructions()
 
-temperature = 0.7
+temperature = 0.8
 
 # initialize the models
 openai = ChatOpenAI(
@@ -38,19 +38,32 @@ openai = ChatOpenAI(
     openai_api_key=openai_key
 )
 
-template="You are a {programmer_type} that writes {language} code to simulate epidemiology compartimental models."
+programmer_type_list = ["college student", "software engineer"]
+language_list = ["python", "fortran"]
+model_list = ["SIR", "SEIR", "SERID", "SIDARTHE"] # could probably make up things and it would make diff eq's for them
+method_list = ["Euler", "odeint", "RK2", "RK3", "RK4"]
+
+template="You are a {programmer_type} that writes {language} code to simulate and plot epidemiology compartimental models."
 system_message_prompt = SystemMessagePromptTemplate.from_template(template)
 human_template="{model} using {method} \n{format_instructions}"
 human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
 
 chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
 
-formatted_prompt = chat_prompt.format_prompt(programmer_type="college student", language="python", model="SIR", method="euler", format_instructions = format_instructions).to_messages()
+formatted_prompt = chat_prompt.format_prompt(programmer_type="college student", language="python", model="SIDARTHE", method="RK3", format_instructions = format_instructions).to_messages()
 
 output = openai(formatted_prompt)
 
 parsed_output = output_parser.parse(output.content)
 
 print(parsed_output['code'])
-print("\nname:")
+print("\n")
 print(parsed_output['function_name'])
+
+with open('output.py', 'w') as f:
+    print(parsed_output['code'], file=f)
+f.close()
+
+with open('output.txt', 'w') as f:
+    print(parsed_output['function_name'], file=f)
+f.close()
